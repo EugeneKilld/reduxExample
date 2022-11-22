a0cae82866b006cb774d8c509e738effa2c84e71
KildibaevEV <dr.jacksik@gmail.com>
2022-11-22 17:55:50 +0300

    базовая разметка

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Button } from "@consta/uikit/Button";
 
 function App() {
-  return (
-  );
+    const [counter, setCounter] = useState(1)
+
+    const onClick = () => {
+        setCounter(counter + 1);
+    };
+
+    return (
+        <div className="center">
+            <Button label="добавить пользователя" onClick={onClick}/>
+            <div>{counter}</div>
+        </div>
+    )
 }
 
 export default App;
